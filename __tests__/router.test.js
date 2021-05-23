086cbb0d048529d5062faa57a1025e2123bc77f2
Harshil Panchal <harshilpanchal00@gmail.com>
2021-05-23 14:55:19 -0700

    finished router test

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,27 @@
  * @jest-environment jsdom
  */
 
+import {pushToHistory} from '../scripts/router.js';
+
+describe('top state is ', () => {
+    test('settings', () => {
+        let hist = pushToHistory('settings');
+        
+        expect(hist.state).toEqual({page: 'settings'});
+        expect(hist.length).toEqual(2);
+    });
+
+    test('entry', () => {
+        let hist = pushToHistory('entry', 5);
+        
+        expect(hist.state).toEqual({page: 'entry5'});
+        expect(hist.length).toEqual(3);
+    });
+    
+    test('other', () => {
+        let hist = pushToHistory('');
+        
+        expect(hist.state).toEqual({});
+        expect(hist.length).toEqual(4);
+    });
+});
